90414cb18f8e702886e1022343ca1b15483264e4
Derek <derekbelloni10@gmail.com>
2022-06-23 13:57:50 -0600

    passing props to tab component

--- app.js
+++ app.js
@@ -1,56 +1,40 @@
-Vue.component('modal', {
+// @ts-ignore
+Vue.component('tabs', {
   template: `
-  <div class="modal is-active">
-  <div class="modal-background"></div>
-  <div class="modal-content">
-    <!-- Any other Bulma elements you want -->
-    <div class="box">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptas eum, itaque ipsa expedita quo
-        nulla ducimus temporibus ea culpa nostrum laudantium, dolores in nihil! Architecto consequatur culpa quis in
-        asperiores possimus dolores optio aliquam a, ipsam, quasi quo obcaecati temporibus magnam nulla eos
-        laudantium
-        hic cumque, excepturi perferendis laborum.</p>
-    </div>
+  <div>
+  <div class="tabs">
+    <ul>
+      <li v-for="tab in tabs">
+        <a href="#"> {{tab.name}}</a>
+      </li>
+    </ul>
   </div>
-  <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
-</div>
-  `
-})
 
-Vue.component('message', {
-  props: ['title', 'body'],
-  template: `
-  <article class="message" v-show="isVisible">
-  <div class="message-header">
-    {{title}}
-    <button class="delete" aria-label="delete" @click="hideCard"></button>
+  <div class="tabs-details">
+    <slot></slot>
   </div>
-  <div class="message-body">
-    {{body}}
   </div>
-</article>
   `,
 
   data() {
-    return {
-      isVisible: true
-    };
+    return { tabs: [] }
   },
-
-  methods: {
-    hideCard() {
-      this.isVisible = !this.isVisible
-    },
-  },
-
-
-})
-
+  created() {
+    this.tabs = this.$children
+  }
+});
 
 // @ts-ignore
-new Vue({
-  el: '#root',
-  data: {
-    showModal: false
+Vue.component('tab', {
+  template: `
+    <div><slot></slot></div>
+  `,
+  props: {
+    name: { required: true },
+    selected: { default: false }
   }
+})
+// @ts-ignore
+new Vue({
+  el: '#root'
 });
