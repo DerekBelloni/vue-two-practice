d258adc30f6130ea96d79ac57cf59b48c0bb153f
Derek <derekbelloni10@gmail.com>
2022-06-25 10:21:15 -0600

    named slots introduced

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-window.Event = new class {
-  constructor() {
-    this.vue = new Vue();
-  }
-
-  fire(event, data = null) {
-    this.vue.$emit(event, data);
-  }
-
-  listen(event, callback) {
-    this.vue.$on(event, callback);
-  }
-}
-
-
-// @ts-ignore
-Vue.component('coupon', {
-  template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied"/>',
-
-  methods: {
-    onCouponApplied() {
-      Event.fire('applied');
-    }
-  }
+Vue.component('modal', {
+  template: `
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+        <slot name="header"></slot>
+      </p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      <slot name="body"></slot>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>
+  `
 })
 
-// @ts-ignore
+
 new Vue({
-  el: '#root',
-
-  data: {
-    couponApplied: false
-  },
-
-  created() {
-    Event.listen('applied', () => alert('Handling event!'))
-  }
-
+  el: '#root'
 })
